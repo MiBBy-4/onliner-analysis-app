57c76b7b10f2f598f81c0c0ade1e147c59aa785d
MiBBy-4 <maksim.762373maks@bk.ru>
2022-01-03 13:24:47 +0300

    feat: add create method in acrticles controller, add errors and messages templates

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,20 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] = "Статья найдена."
+      redirect_to root_path
+    else
+      render "new"
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:link)
+  end
 end
