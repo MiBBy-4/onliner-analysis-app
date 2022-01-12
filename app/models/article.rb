397e5452b216f476769a7fc345fa159154e7debe
MiBBy-4 <maksim.762373maks@bk.ru>
2022-01-12 11:02:24 +0300

    feat: create new comments table with comments and username columns

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ApplicationRecord
   validates :link, presence: true, length: { maximum: 500 }, url: { accept_array: true }
-  title :string, length: { minimum:3 }
-  
+  validates :title, length: { minimum:3 }
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_12_073103) do
+ActiveRecord::Schema.define(version: 2022_01_12_074341) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 2022_01_12_073103) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "title"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text "comment"
+    t.string "username"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
