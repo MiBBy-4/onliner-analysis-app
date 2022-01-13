1f632348b015d06fe61d80efbe69b3ec64b515f4
MiBBy-4 <maksim.762373maks@bk.ru>
2022-01-13 13:38:54 +0300

    refactor: rename comment column to content in comments table

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord
   validates :link, presence: true, length: { maximum: 500 }, url: { accept_array: true }
-  validates :title, length: { minimum:3 }
+  validates :title, length: { minimum: 3 }
 
   has_many :article_comments
   has_many :comments, through: :article_comments
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_12_080443) do
+ActiveRecord::Schema.define(version: 2022_01_13_103637) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,7 +30,7 @@ ActiveRecord::Schema.define(version: 2022_01_12_080443) do
   end
 
   create_table "comments", force: :cascade do |t|
-    t.text "comment"
+    t.text "content"
     t.string "username"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
