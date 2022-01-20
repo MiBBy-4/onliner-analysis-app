b78015ec434b333d75f66db95c9e4674a33ef32f
MiBBy-4 <maksim.762373maks@bk.ru>
2022-01-20 13:50:36 +0300

    refactor: move url in const in RateCommentService, delete indentation RateCommentService

--- app/services/rate_comment_service.rb
+++ app/services/rate_comment_service.rb
@@ -1,19 +1,19 @@
 class RateCommentService < ApplicationService
+  URL = "https://language.googleapis.com/v1beta2/documents:analyzeSentiment?key=AIzaSyARfeAHElRfkGkT8FXOtrVzBYNPXLG3WFY"
   def initialize(content)
     @content = content
   end
 
   def call
-    response = HTTPX.post('https://language.googleapis.com/v1beta2/documents:analyzeSentiment?key=AIzaSyARfeAHElRfkGkT8FXOtrVzBYNPXLG3WFY', json:{
+    response = HTTPX.post(URL, json:{
       document: {
         language: "en",
-        content: "#{@content}",
+        content: @content,
         type: "PLAIN_TEXT"
       },
       encodingType: "NONE"
     }
   )
-
   data_hash = JSON.parse(response.body)
   document_sentiment = data_hash['documentSentiment']
   document_sentiment['score'].to_f*100
