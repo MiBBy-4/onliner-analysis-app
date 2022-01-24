82ca16474a3b687463120385ac9bfdb136f315ab
MiBBy-4 <maksim.762373maks@bk.ru>
2022-01-24 15:55:22 +0300

    feat: add information output of articles and comments in index and show templates

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,9 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only:[:show]
 
-  def index; end
+  def index
+    @articles = Article.all()
+  end
 
   def new
     @article = Article.new
